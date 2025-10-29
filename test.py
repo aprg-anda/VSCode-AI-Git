92ee4bd4f5c85cba20e2e4386d6dde7e061c41c6
aprg-anda <aprg@andapresent.hu>
2025-10-29 10:11:04 +0000

    Oct 29, 2025, 11:11 AM

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 username = input("Enter your username: ")
 age = input("Enter your age: ")
-if not age.isdigit() or int(age) <= 0:
+while not age.isdigit() or int(age) <= 0:
     print("Invalid age. Please enter a positive number.")
+    age = input("Enter your age: ")
+
+if int(age) < 18:
+    print("Hi " + username + "! Welcome to the program.")
+elif int(age) < 36:
+    print("Hello " + username + "! Welcome to the program.")
+elif int(age) < 54:
+    print("Welcome " + username + "! Welcome to the program.")
+elif int(age) < 72:
+    print("It is a pleasure to meet you, " + username + "! Welcome to the program.")
 else:
-    if int(age) < 18:
-        print("Hi " + username + "! Welcome to the program.")
-    elif int(age) < 36:
-        print("Hello " + username + "! Welcome to the program.")
-    elif int(age) < 54:
-        print("Welcome " + username + "! Welcome to the program.")
-    elif int(age) < 72:
-        print("It is a pleasure to meet you, " + username + "! Welcome to the program.")
-    else:
-        print("I welcome thee, " + username + "! Welcome to the program.")
+    print("I welcome thee, " + username + "! Welcome to the program.")
